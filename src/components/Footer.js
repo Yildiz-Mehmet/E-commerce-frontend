d2ce33c05e71440ede4bb1bbce8636525a3c5a4e
Yildiz-Mehmet <mesel4461@gmail.com>
2024-07-08 10:46:47 +0300

    ok

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-   <footer>
-    <p></p>
+   <footer className='bg-slate-200'>
+    <div className='container mx-auto p-4'>
+      <p className='text-center'>Dynamic Coding with Amit</p>
+    </div>
    </footer>
   )
 }
