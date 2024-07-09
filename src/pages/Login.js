7b42a8dfe76ef66f3ad66bb6d3369193d3cf1aee
Yildiz-Mehmet <mesel4461@gmail.com>
2024-07-09 22:35:55 +0300

    ok

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
             <div className='w-20 h-20 mx-auto'>
                 <img src={loginIcons} alt='login icons' />
             </div>
-            <form className='pt-6' onSubmit={handleSubmit}>
+            <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
                 <div className='grid'>
                     <label>Email : </label>
                     <div className='bg-slate-100 p-2'>
