9e8cc23de9a3b53178b561e53b8c025733016bc5
Yildiz-Mehmet <mesel4461@gmail.com>
2024-07-11 22:08:37 +0300

    ok

---
+++ src/common/index.js
@@ -0,0 +1,11 @@
+const backendDomin = "http://localhost:8080"
+
+
+const SummaryApi = {
+    signUp :{
+        url: `${backendDomin}/api/signup`,
+        method : "post"
+    }
+}
+
+export default SummaryApi
